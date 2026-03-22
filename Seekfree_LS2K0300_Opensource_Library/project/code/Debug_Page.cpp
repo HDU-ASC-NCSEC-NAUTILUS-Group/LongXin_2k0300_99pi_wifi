5cb71f5d76c477b38ee09d73a2cba82aa4f31a19
YINGYYUN <756209834@qq.com>
2026-03-22 17:14:53 +0800

    feat:2026.3.22.17:14:暂存一个中间版本

--- Seekfree_LS2K0300_Opensource_Library/project/code/Debug_Page.cpp
+++ Seekfree_LS2K0300_Opensource_Library/project/code/Debug_Page.cpp
@@ -2,6 +2,7 @@
 #include "zf_common_headfile.h"
 #include "defines.h"
 #include "Key.h"
+#include "MOTOR.h"
 
 
 /*******************************************************************************************************************/
@@ -18,10 +19,29 @@ void Debug_Page_Menu_UI(uint8_t Page)
 			ips200_show_string(8  , 0  , "[Debug]");
 			ips200_show_string(0  , 16 , "==============================");
 			ips200_show_string(10 , 32 , "BUZ");
-            ips200_show_string(10 , 48 , "PWM+ENCODER");
+            ips200_show_string(10 , 48 , "MOTOR");
 		
 			break;
 	}
+}
+
+// [三级界面]蜂鸣器调试界面
+void Debug_BUZ_UI(void)
+{
+    ips200_show_string(8  , 0  , "[DEBUG]-BUZ");
+    ips200_show_string(0  , 16 , "==============================");
+    ips200_show_string(10 , 32 , "OFF");
+}
+
+// [三级界面]电机调试界面
+void Debug_MOTOR_UI(void)
+{
+    ips200_show_string(8  , 0  , "[DEBUG]-MOTOR");
+    ips200_show_string(0  , 16 , "==============================");
+    ips200_show_string(10 , 32 , "PWM1:#####      ENC1:###");
+    ips200_show_string(10 , 48 , "PWM2:#####      ENC2:###");
+    ips200_show_string(10 , 64 , "PWM3:#####      ENC3:###");
+    ips200_show_string(10 , 80 , "PWM4:#####      ENC4:###");
 }
 /*******************************************************************************************************************/
 /*--------------------------------------------------------------------------------------------------[E] 菜单样式 [E]*/
@@ -34,12 +54,13 @@ void Debug_Page_Menu_UI(uint8_t Page)
 
 // 相关函数提前声明
 int Debug_BUZ       (void);
+int Debug_MOTOR       (void);
 
 // [二级界面]Debug模式界面
 int Debug_Page_Menu(void)
 {
     // Debug模式选项 标志位
-     uint8_t Debug_Page_flag = 1;
+    uint8_t Debug_Page_flag = 1;
 
     Debug_Page_Menu_UI(1);
     ips200_show_string(0  ,32 , ">");
@@ -51,18 +72,19 @@ int Debug_Page_Menu(void)
 		// 上/下按键是否被按下过
 		uint8_t key_pressed = 0;
 
+
         /* 按键处理*/
         if (Key_Check(KEY_NAME_UP,KEY_SINGLE))
         {
             key_pressed = 1;
             Debug_Page_flag --;
-            if (Debug_Page_flag < 1)Debug_Page_flag = 1;
+            if (Debug_Page_flag < 1)Debug_Page_flag = 2;
         }
         else if (Key_Check(KEY_NAME_DOWN,KEY_SINGLE))
         {
             key_pressed = 1;
             Debug_Page_flag ++;
-            if (Debug_Page_flag > 1)Debug_Page_flag = 1;
+            if (Debug_Page_flag > 2)Debug_Page_flag = 1;
         }
         else if (Key_Check(KEY_NAME_CONFIRM,KEY_SINGLE))
         {
@@ -86,6 +108,17 @@ int Debug_Page_Menu(void)
             Debug_Page_Menu_UI(1);
             ips200_show_string(0  ,32 , ">");
         }
+        else if (Debug_Page_flag_temp == 2)
+        {
+            ips200_clear();
+            Debug_MOTOR();
+            
+            // 从子界面返回后
+            ips200_clear();
+            Debug_Page_Menu_UI(1);
+            ips200_show_string(0  ,48 , ">");
+        }
+        
 
         /* 显示更新*/
         if (key_pressed)
@@ -111,16 +144,12 @@ int Debug_Page_Menu(void)
 // [三级界面]蜂鸣器调试
 int Debug_BUZ(void)
 {
-    ips200_show_string(8  , 0  , "[DEBUG]-BUZ");
-    ips200_show_string(0  , 16 , "==============================");
-    ips200_show_string(10 , 32 , "OFF");
-
+    Debug_BUZ_UI();
     uint8_t BUZ_flag = 0;
 
     while(1)
     {
-
-        // 按键处理
+        /* 按键处理*/   
         if (Key_Check(KEY_NAME_CONFIRM,KEY_SINGLE)) 
         {
             BUZ_flag = 1 - BUZ_flag;
@@ -143,9 +172,127 @@ int Debug_BUZ(void)
             return 0;   
         }
     }
-
 }
- 
+
+// #   #   ###   #####   ###   #####  ####   
+// ## ##  #   #    #    #   #    #    #   #  
+// # # #  #   #    #    #   #    #    ####   
+// #   #  #   #    #    #   #    #    #  #   
+// #   #   ###     #     ###     #    #   #  
+//
+// [三级界面]电机调试
+int Debug_MOTOR(void)
+{  
+    // 电机调试 标志位
+    uint8_t Debug_MOTOR_flag = 0;
+
+    Debug_MOTOR_UI();
+    ips200_show_string(0  ,32 , ">");
+
+    while(1)
+    {
+        // 选择模式（无选中目标）
+        if (Debug_MOTOR_flag_temp == 0)
+        {
+            // 存储确认键被按下时Debug_MOTOR_flag的值的临时变量，默认为无效值0
+            uint8_t Debug_MOTOR_flag_temp = 0;
+            // 上/下按键是否被按下过
+            uint8_t key_pressed = 0;
+            
+            
+            /* 按键处理*/       
+            if (Key_Check(KEY_NAME_UP,KEY_SINGLE)) 
+            {
+                Debug_MOTOR_flag --;
+                if (Debug_MOTOR_flag < 1)Debug_MOTOR_flag = 4;
+                key_pressed = 1;           
+            }
+            else if (Key_Check(KEY_NAME_DOWN,KEY_SINGLE)) 
+            {
+                Debug_MOTOR_flag ++;
+                if (Debug_MOTOR_flag > 4)Debug_MOTOR_flag = 1;
+                key_pressed = 1;
+            }
+            else if (Key_Check(KEY_NAME_CONFIRM,KEY_SINGLE)) 
+            {
+                Debug_MOTOR_flag_temp = Debug_MOTOR_flag;       
+            }
+            else if (Key_Check(KEY_NAME_BACK,KEY_SINGLE))
+            {
+                // 返回上一级界面
+                return 0;   
+            }
+
+
+            /* 光标变化*/
+            if (Debug_MOTOR_flag_temp == 1)
+            {
+                ips200_show_string(0  ,32 , "=");
+            }
+            else if (Debug_MOTOR_flag_temp == 2)
+            {
+                ips200_show_string(0  ,48 , "=");             
+            }
+            else if (Debug_MOTOR_flag_temp == 3)
+            {
+                ips200_show_string(0  ,64 , "=");                  
+            }
+            else if (Debug_MOTOR_flag_temp == 4)
+            {
+                ips200_show_string(0  ,80 , "=");
+            }
+        }
+        // 更改模式（有选中目标）
+        else if (Debug_MOTOR_flag_temp != 0)
+        {
+            /* 按键处理*/       
+            if (Key_Check(KEY_NAME_UP,KEY_SINGLE)) 
+            {
+                key_pressed = 1;           
+            }
+            else if (Key_Check(KEY_NAME_DOWN,KEY_SINGLE)) 
+            {
+                key_pressed = 1;
+            }
+            else if (Key_Check(KEY_NAME_CONFIRM,KEY_SINGLE)) 
+            {
+
+            }
+            else if (Key_Check(KEY_NAME_BACK,KEY_SINGLE))
+            {
+                // 返回上一级界面
+                return 0;   
+            }
+        }
+
+
+        /* 显示更新*/
+        if (key_pressed)
+        {
+            // 清除光标，暂时用这个方法
+            ips200_show_string(0  ,32 , " ");
+            ips200_show_string(0  ,48 , " ");
+            ips200_show_string(0  ,64 , " ");
+            ips200_show_string(0  ,80 , " ");
+
+            switch (Debug_MOTOR_flag)
+            {
+                case 1:
+                    ips200_show_string(0  ,32 , ">");
+                    break;
+                case 2:
+                    ips200_show_string(0  ,48 , ">");
+                    break;
+                case 3:
+                    ips200_show_string(0  ,64 , ">");
+                    break;
+                case 4:
+                    ips200_show_string(0  ,80 , ">");
+                    break;
+            }
+        }
+    }
+}
 
 /*******************************************************************************************************************/
 /*--------------------------------------------------------------------------------------------------[E] 交互界面 [E]*/
--- Seekfree_LS2K0300_Opensource_Library/project/code/defines.h
+++ Seekfree_LS2K0300_Opensource_Library/project/code/defines.h
@@ -18,11 +18,7 @@
 // 蜂鸣器宏定义
 #define BEEP_DEFINE         "/dev/zf_driver_gpio_beep"  
 
-// 电机相关定义
-#define MOTOR1_DEFINE_DIR   "/dev/zf_driver_gpio_motor_1"
-#define MOTOR2_DEFINE_DIR   "/dev/zf_driver_gpio_motor_2"
-#define MOTOR1_DEFINE_PWM   "/dev/zf_device_pwm_motor_1"
-#define MOTOR2_DEFINE_PWM   "/dev/zf_device_pwm_motor_2"
+
 
 
 #endif
